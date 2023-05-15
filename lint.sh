46d86934bc693546d25ecddef4866240616d2b33
Dan Luhring <dluhring@chainguard.dev>
2023-05-15 16:20:55 -0400

    Add lint check to ban advisories data
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

--- lint.sh
+++ lint.sh
@@ -11,4 +11,14 @@ for f in *.yaml; do
     echo "missing $want in packages.txt"
     exit 1
   fi
+
+  # With the introduction of https://github.com/chainguard-dev/enterprise-advisories,
+  # package config files should no longer contain any advisory data.
+  if [[ "$(yq 'keys | contains(["advisories"])' "$f")" == "true" ]]; then
+    echo "
+$f has an 'advisories' section, but advisory data should now be stored in https://github.com/chainguard-dev/enterprise-advisories.
+
+To learn about how to create advisory data in the advisories repo, run 'wolfictl advisory create -h', and check out the '--advisories-repo-dir' flag."
+    exit 1
+  fi
 done
