5392f207d21734d38a55dd188df252df81ed9447
Mauren <698465+stormqueen1990@users.noreply.github.com>
2024-01-04 12:43:36 -0500

    chore: remove obsolete configuration from OpenJDK BCFIPS tests (#1158)
    Signed-off-by: Mauren Berti <mauren.berti@chainguard.dev>

--- openjdk-bcfips/test.sh
+++ openjdk-bcfips/test.sh
@@ -6,13 +6,5 @@ JAVA_HOME="${DESTDIR}/${JAVA_HOME}"
 CLASSPATH="/usr/share/java/bouncycastle-fips/*:."
 export CLASSPATH
 
-JDK_JAVA_OPTIONS="-Djava.security.properties==${JAVA_HOME}/conf/security/java.security"
-JDK_JAVA_OPTIONS="${JDK_JAVA_OPTIONS} --add-exports java.base/sun.security.internal.spec=ALL-UNNAMED"
-export JDK_JAVA_OPTIONS
-
-JDK_JAVA_OPTIONS="${JDK_JAVA_OPTIONS} -Djavax.net.ssl.trustStore=cacerts.bcfks"
-JDK_JAVA_OPTIONS="${JDK_JAVA_OPTIONS} -Djavax.net.ssl.trustStorePassword=changeit"
-export JDK_JAVA_OPTIONS
-
 "${JAVA_HOME}/bin/javac" Test.java
 "${JAVA_HOME}/bin/java" Test
